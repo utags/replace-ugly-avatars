fd5f5e98e9591a2775e0c6bb925956f5b6913fc2
pipecraft <pipecraft.net@gmail.com>
2025-03-28 21:08:24 +0800

    0.5.1

--- build/userscript-prod/replace-ugly-avatars.user.js
+++ build/userscript-prod/replace-ugly-avatars.user.js
@@ -4,7 +4,7 @@
 // @namespace            https://github.com/utags/replace-ugly-avatars
 // @homepageURL          https://github.com/utags/replace-ugly-avatars#readme
 // @supportURL           https://github.com/utags/replace-ugly-avatars/issues
-// @version              0.5.0
+// @version              0.5.1
 // @description          🔃 Replace specified user's avatar (profile photo) and username (nickname)
 // @description:zh-CN    🔃 换掉别人的头像与昵称
 // @icon                 data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%230d6efd' class='bi bi-arrow-repeat' viewBox='0 0 16 16'%3E %3Cpath d='M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z'/%3E %3Cpath fill-rule='evenodd' d='M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z'/%3E %3C/svg%3E
@@ -1354,7 +1354,15 @@
     matches: /^linux\.do$/,
     getAvatarElements() {
       return $$(
-        'img[src^="https://linux.do/user_avatar/linux.do/"],img[src^="/user_avatar/linux.do/"],img[src^="https://linux.do/letter_avatar_proxy/"],img[src^="/letter_avatar_proxy/"],img[data-rua-org-src]'
+        [
+          'img[src^="https://linux.do/user_avatar/linux.do/"]',
+          'img[src^="https://cdn.linux.do/user_avatar/linux.do/"]',
+          'img[src^="/user_avatar/linux.do/"]',
+          'img[src^="https://linux.do/letter_avatar_proxy/"]',
+          'img[src^="https://cdn.linux.do/letter_avatar_proxy/"]',
+          'img[src^="/letter_avatar_proxy/"]',
+          "img[data-rua-org-src]",
+        ].join(",")
       )
     },
     getUserName(element) {
