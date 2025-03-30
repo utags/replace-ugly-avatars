e9436b48bde431bb164484f672ecd858e74df6ef
pipecraft <pipecraft.net@gmail.com>
2025-03-30 10:28:48 +0800

    0.5.3

--- build/userscript-prod/replace-ugly-avatars.user.js
+++ build/userscript-prod/replace-ugly-avatars.user.js
@@ -4,7 +4,7 @@
 // @namespace            https://github.com/utags/replace-ugly-avatars
 // @homepageURL          https://github.com/utags/replace-ugly-avatars#readme
 // @supportURL           https://github.com/utags/replace-ugly-avatars/issues
-// @version              0.5.2
+// @version              0.5.3
 // @description          🔃 Replace specified user's avatar (profile photo) and username (nickname)
 // @description:zh-CN    🔃 换掉别人的头像与昵称
 // @icon                 data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%230d6efd' class='bi bi-arrow-repeat' viewBox='0 0 16 16'%3E %3Cpath d='M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z'/%3E %3Cpath fill-rule='evenodd' d='M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z'/%3E %3C/svg%3E
@@ -1378,6 +1378,10 @@
           /.*\/letter_avatar_proxy\/v4\/letter\/(\w\/[^/]+)\/.*/,
           "$1"
         )
+        return name2.toLowerCase()
+      }
+      if (src.includes("letter_avatar/")) {
+        const name2 = src.replace(/.*\/letter_avatar\/(\w+)\/.*/, "$1")
         return name2.toLowerCase()
       }
       const name = src.replace(/.*\/user_avatar\/linux\.do\/([^/]+)\/.*/, "$1")
